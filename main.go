7b087a001eabf3aa7b0b330601c88ef9a5ebb7ed
Hakan EVKURAN <49947902+bcfmhakan@users.noreply.github.com>
2021-12-08 00:35:41 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,32 +1,22 @@
 package main
 
 import (
-	"net/http"
-	"os"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+    "fmt"
+    "html"
+    "log"
+    "net/http"
 )
 
 func main() {
 
-	e := echo.New()
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    })
 
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hi")
+    })
 
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello, Docker! <3")
-	})
+    log.Fatal(http.ListenAndServe(":8081", nil))
 
-	e.GET("/ping", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
-	})
-
-	httpPort := os.Getenv("HTTP_PORT")
-	if httpPort == "" {
-		httpPort = "8080"
-	}
-
-	e.Logger.Fatal(e.Start(":" + httpPort))
 }
