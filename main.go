a1437e1bae17faecf5099962a5999f47e3c5e630
Hakan EVKURAN <49947902+bcfmhakan@users.noreply.github.com>
2021-12-10 22:16:01 +0300

    Update main.go

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ import (
     "log"
     "net/http"
     "github.com/prometheus/client_golang/prometheus/promhttp"
-    "github.com/etherlabsio/healthcheck/v2"
-    "github.com/etherlabsio/healthcheck/v2/checkers"
 )
 
 func main() {
@@ -23,28 +21,5 @@ func main() {
     // Prometheus endpoint
     http.Handle("/metrics", promhttp.Handler())
     log.Fatal(http.ListenAndServe(":8081", nil))
-    r := mux.NewRouter()
-    r.Handle("/healthcheck", healthcheck.Handler(
 
-        // WithTimeout allows you to set a max overall timeout.
-        healthcheck.WithTimeout(5*time.Second),
-
-        // Checkers fail the status in case of any error.
-        healthcheck.WithChecker(
-            "heartbeat", checkers.Heartbeat("$PROJECT_PATH/heartbeat"),
-        ),
-
-        healthcheck.WithChecker(
-            "database", healthcheck.CheckerFunc(
-                func(ctx context.Context) error {
-                    return db.PingContext(ctx)
-                },
-            ),
-        ),
-
-        // Observers do not fail the status in case of error.
-        healthcheck.WithObserver(
-            "diskspace", checkers.DiskSpace("/var/log", 90),
-        ),
-    ))
 }
