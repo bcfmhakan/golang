08b9d8fe47603b9d6113019d38e2c819d76f5bc8
Hakan EVKURAN <49947902+bcfmhakan@users.noreply.github.com>
2021-12-12 18:59:39 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
-import (
+import 
+    "html/template"
     "fmt"
     "html"
     "log"
@@ -10,9 +11,13 @@ import (
 
 func main() {
 
+    
+    // An HTML template
+
+        
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         
-        fmt.Fprintf(w, "<img src='https://media-exp1.licdn.com/dms/image/C4D22AQF-oJhwPSSQNQ/feedshare-shrink_1280/0/1638953672281?e=1642032000&v=beta&t=veoZsqkTUV9edSrTlyIQJqX4lzaH44kHBShXwVudQrQ' >", html.EscapeString(r.URL.Path))
+        fmt.Fprintf(w, <img src='https://media-exp1.licdn.com/dms/image/C4D22AQF-oJhwPSSQNQ/feedshare-shrink_1280/0/1638953672281?e=1642032000&v=beta&t=veoZsqkTUV9edSrTlyIQJqX4lzaH44kHBShXwVudQrQ' >, html.EscapeString(r.URL.Path))
     })
 
     http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
