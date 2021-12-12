6cfc0d223234a9750b749cb4a79c6d66de74a8c4
Hakan EVKURAN <49947902+bcfmhakan@users.noreply.github.com>
2021-12-12 19:16:10 +0300

    Update main.go

--- main.go
+++ main.go
@@ -17,10 +17,10 @@ func main() {
         
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         
-        fmt.Fprintf(w, <img src='https://media-exp1.licdn.com/dms/image/C4D22AQF-oJhwPSSQNQ/feedshare-shrink_1280/0/1638953672281?e=1642032000&v=beta&t=veoZsqkTUV9edSrTlyIQJqX4lzaH44kHBShXwVudQrQ' >, html.EscapeString(r.URL.Path))
+        fmt.Fprintf(w,"#BC4M", html.EscapeString(r.URL.Path))
     })
 
-    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
+    http.HandleFunc("/bc4m", func(w http.ResponseWriter, r *http.Request){
         fmt.Fprintf(w, "Hi")
     })
 
