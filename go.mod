20757b0b47988d1a2d503deaec5138b835f9cb24
Batuhan Harun Demirci <harun@Batuhan-MacBook-Pro.local>
2021-12-16 10:47:18 +0300

    new spa

---
+++ go.mod
@@ -0,0 +1,19 @@
+module bcfm.test
+
+go 1.17
+
+require github.com/nelkinda/health-go v0.0.1
+
+require (
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.9.5 // indirect
+	github.com/nelkinda/http-go v0.0.1 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v0.0.0-20180714160509-73f8eece6fdc // indirect
+	go.mongodb.org/mongo-driver v1.3.4 // indirect
+	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9 // indirect
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58 // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,26 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "html"
-    "log"
-    "net/http"
-    "github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+
+	"github.com/nelkinda/health-go"
+	"github.com/nelkinda/health-go/checks/sysinfo"
 )
 
 func main() {
-
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        
-        fmt.Fprintf(w, "<img src='https://media-exp1.licdn.com/dms/image/C4D22AQF-oJhwPSSQNQ/feedshare-shrink_1280/0/1638953672281?e=1642032000&v=beta&t=veoZsqkTUV9edSrTlyIQJqX4lzaH44kHBShXwVudQrQ' >", html.EscapeString(r.URL.Path))
-    })
-
-    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Hi")
-    })
-
-    // Prometheus endpoint
-    http.Handle("/metrics", promhttp.Handler())
-    log.Fatal(http.ListenAndServe(":8081", nil))
-
+	h := health.New(
+		health.Health{
+			Version:   "1",
+			ReleaseID: "1.0.0",
+		},
+		sysinfo.Health(),
+	)
+	http.HandleFunc("/health", h.Handler)
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.ListenAndServe(":3000", nil)
 }
